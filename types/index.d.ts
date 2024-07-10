0cc41a902d34a2c58af853f8764a77a7ee2435c2
Aryan Jagarwal <aryanjagarwal@Aryans-MacBook-Air.local>
2024-07-11 00:26:00 +0530

    middleware

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,8 +5,8 @@ declare type CreateUserParams = {
     clerkId: string;
     email: string;
     username: string;
-    firstName: string;
-    lastName: string;
+    firstName: string | null; // my code
+    lastName: string | null;
     photo: string;
   };
   
